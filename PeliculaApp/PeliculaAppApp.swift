38761e1df5435c929f9105c9662bcf92712b969b
Danym2407 <danym2407@gmail.com>
2025-02-21 16:30:38 -0600

    Initial Commit

---
+++ PeliculaApp/PeliculaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeliculaAppApp.swift
+//  PeliculaApp
+//
+//  Created by Daniela Mendez on 21/02/25.
+//
+
+import SwiftUI
+
+@main
+struct PeliculaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

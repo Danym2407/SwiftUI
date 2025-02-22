e18820a0c5e6425a93f12fa24318768f958fa6e7
Danym2407 <danym2407@gmail.com>
2025-02-21 18:01:10 -0600

    Agregamos nuestro codigo que nos permite calcular la propina

--- CalculadoraPropina/ContentView.swift
+++ CalculadoraPropina/ContentView.swift
@@ -8,14 +8,71 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var monto = "1000"
+    @State var porcentaje = 10.0
+    
+    var totalPropina: Double{
+        let total = Double(monto) ?? 0
+        let propina = (total * porcentaje)/100
+        return total+propina;
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        VStack{
+            
+            
+            HStack {
+                Image(systemName: "dollarsign.arrow.trianglehead.counterclockwise.rotate.90")
+                    .imageScale(.large)
+                    .foregroundColor(.green)
+                    .bold()
+                
+                Text("Calculadora de Propina")
+                    .bold()
+                    .padding()
+                
+                Image(systemName: "banknote.fill")
+                    .imageScale(.large)
+                    .foregroundColor(.green)
+                    .bold()
+                
+            }
+            .padding()
+            
+            
+            
+            
+            HStack{
+                Text("$")
+                TextField("Monto", text: $monto)
+                Text(monto)
+            }.padding()
+            
+            
+            HStack{
+                Slider(value: $porcentaje, in: 5...100, step: 5)
+            }
+            .padding()
+            
+            HStack{
+                Text("Total a pagar: $\(totalPropina, specifier: "%.2f")")
+                
+            }
+            .padding()
+            
+            
+            
+            
+            
+            Spacer()
+            
         }
         .padding()
+        
+        
+        
     }
 }
 
